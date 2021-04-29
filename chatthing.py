45d7e6c893f95bcfa409afb0b4e830b20dab91b3
zTheroy <83382093+zTheroy@users.noreply.github.com>
2021-04-28 21:03:25 -0500

    chatthing.py
    uploading chatthing.py to main

---
+++ chatthing.py
@@ -0,0 +1,57 @@
+import os, random, discord
+from dotenv import load_dotenv
+from urllib.request import urlopen, Request
+
+discord_token = "secret"
+
+load_dotenv()
+
+TOKEN = 'secret'
+print(TOKEN)
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("We have logged  in as {}".format(client))
+
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    random_quotes = [
+        'bro this bot took too long',
+        'so guys in this tutorial im gonna teach you how to be funny',
+        (
+            'thanks to lam 2 '
+            'this took 1~ months (because i barley programed LOL! XD! LMAO!)'
+        ),
+    ]
+
+    if message.content == 't!randomquotes':
+        response = random.choice(random_quotes)
+        await message.channel.send(response)
+
+    if message.content == 't!randommath':
+        x = random.randint(0,101)
+        y = random.randint(0,101)
+        answer = x + y
+        math_output = '{} + {} = {}'.format(x,y,answer) 
+        await message.channel.send(math_output)
+##    if message.content == 't!numberguess':
+##        guess = int(input('Guess youre number: ')
+    if message.content == 't!inspiration':
+        random_quote = Request('https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?', headers={'User-Agent': 'Mozilla/5.0'})
+        page = urlopen(random_quote)
+        html_bytes = page.read()
+        html = html_bytes.decode("utf-8")
+        #print(html)
+        html = html.split(":")
+        inspiration = html[1].split("quoteAuthor")
+        inspiration = inspiration[0]
+        inspiration = inspiration[:-3]
+        await message.channel.send(inspiration)
+client.run(TOKEN)
