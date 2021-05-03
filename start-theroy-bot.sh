eb9339683008a53f3ee5478955ad85578a519e53
zTheroy <83382093+zTheroy@users.noreply.github.com>
2021-05-02 19:46:43 -0500

    Delete start-theroy-bot.sh

--- start-theroy-bot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Eddies script to launch Discord bot
-
-pkill chatthing.py
-
-
-nohup python3.7 chatthing.py "ODIwNDM1MDI5MTI5ODIyMjk4.YE1HqQ.HOlALa-dNWuky7vv4031XX_mXZM" >/dev/null 2>&1 &
