13dd272e5ba010f5e93231eaf63b83e4f32a7974
--Lol-- <11202351+specters312@users.noreply.github.com>
2021-05-16 00:10:33 -0700

    Update t!help
    added t!cvf to help

--- chatthing.py
+++ chatthing.py
@@ -62,6 +62,7 @@ def main(argv):
             info.add_field(name="t!inspiration", value="makes you go :O", inline=False)
             info.add_field(name="t!randommath", value="random addition. adds numbers 1-100", inline=False)
             info.add_field(name="t!randomquotes", value="random quotes, made by yours truly", inline=False)
+            info.add_field(name="t!cvf y=mx+b", value="replace y=mx+b with numbers (Don't make y or x a number, and don't add spaces)", inline=False)
             await message.channel.send(embed=info)
 
 
