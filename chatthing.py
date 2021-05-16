2e6d24d15887eb410956125dd1c883f27da79bf8
zTheroy <83382093+zTheroy@users.noreply.github.com>
2021-05-15 23:27:07 -0500

    (Hopefully) Fixed the embed command
    please god

--- chatthing.py
+++ chatthing.py
@@ -27,7 +27,7 @@ def main(argv):
     @client.event
     async def embed(ctx):
         embed=discord.Embed(title="Sample Embed", url="httpfs://realdrewdata.medium.com/", description="This is an embed that will show how to build an embed and the different components", color=0xFF5733)
-        await ctx.send(embed=embed)
+        await message.channel.send(t!embed)
 
 
     @client.event
