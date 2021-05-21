fa1db2e94109ddb40b4ccbf76f8047eac3dd36e1
zTheroy <83382093+zTheroy@users.noreply.github.com>
2021-05-21 00:07:10 -0500

    updated mention thiingy

--- chatthing.py
+++ chatthing.py
@@ -89,8 +89,11 @@ def main(argv):
             else:
                 cvf_answer="Youre ansssswer do be: {} + {} = {} :flushed:".format(mx, y, answer)
             await message.channel.send(cvf_answer)
-        if message.content == "<@820435029129822298>":
-            await message.channel.send(embed=info)
+    @bot.event
+    async def on_message(message):
+    mention = f'<@!{bot.user.id}>'
+        if mention in message.content:
+            await message.channel.send("You mentioned me")
                 
     client.run(TOKEN)
 if __name__ == "__main__":
