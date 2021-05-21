1819d645afa4aad2e384c6abcf8d521135c4a505
zTheroy <83382093+zTheroy@users.noreply.github.com>
2021-05-20 23:38:38 -0500

    Update chatthing.py

--- chatthing.py
+++ chatthing.py
@@ -40,59 +40,59 @@ def main(argv):
             ),
         ]
 
-        if message.content == 't!randomquotes':
-            response = random.choice(random_quotes)
-            await message.channel.send(response)
+    if message.content == 't!randomquotes':
+        response = random.choice(random_quotes)
+        await message.channel.send(response)
 
-        if message.content == 't!randommath':
-            x = random.randint(0,101)
-            y = random.randint(0,101)
-            answer = x + y
-            math_output = '{} + {} = {}'.format(x,y,answer)
-            await message.channel.send(math_output)
+    if message.content == 't!randommath':
+        x = random.randint(0,101)
+        y = random.randint(0,101)
+        answer = x + y
+        math_output = '{} + {} = {}'.format(x,y,answer)
+        await message.channel.send(math_output)
     ##    if message.content == 't!numberguess':
     ##        guess = int(input('Guess youre number: ')
-        if message.content == 't!inspiration':
-            random_quote = Request('https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?', headers={'User-Agent': 'Mozilla/5.0'})
-            page = urlopen(random_quote)
-            html_bytes = page.read()
-            html = html_bytes.decode("utf-8")
+    if message.content == 't!inspiration':
+        random_quote = Request('https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?', headers={'User-Agent': 'Mozilla/5.0'})
+        page = urlopen(random_quote)
+        html_bytes = page.read()
+        html = html_bytes.decode("utf-8")
             #print(html)
-            html = html.split(":")
-            inspiration = html[1].split("quoteAuthor")
-            inspiration = inspiration[0]
-            inspiration = inspiration[:-3]
-            await message.channel.send(inspiration)
-        if message.content == 't!help':
-            info = discord.Embed(title="Commands list:",  description="All existing commands", color=0x28e038)
-            info.add_field(name="t!inspiration", value="makes you go :O", inline=False)
-            info.add_field(name="t!randommath", value="random addition. adds numbers 1-100", inline=False)
-            info.add_field(name="t!randomquotes", value="random quotes, made by yours truly", inline=False)
-            info.add_field(name="t!cvf y=mx+b", value="replace y=mx+b with numbers (Don't nake y a number, and don't add spaces)", inline=False)
-            await message.channel.send(embed=info)
+        html = html.split(":")
+        inspiration = html[1].split("quoteAuthor")
+        inspiration = inspiration[0]
+        inspiration = inspiration[:-3]
+        await message.channel.send(inspiration)
+    if message.content == 't!help':
+        info = discord.Embed(title="Commands list:",  description="All existing commands", color=0x28e038)
+        info.add_field(name="t!inspiration", value="makes you go :O", inline=False)
+        info.add_field(name="t!randommath", value="random addition. adds numbers 1-100", inline=False)
+        info.add_field(name="t!randomquotes", value="random quotes, made by yours truly", inline=False)
+        info.add_field(name="t!cvf y=mx+b", value="replace y=mx+b with numbers (Don't nake y a number, and don't add spaces)", inline=False)
+        await message.channel.send(embed=info)
 
 
-        if message.content.startswith('t!cvf'):
-            message_content = message.content.split()
-            arg = message_content[1]
+    if message.content.startswith('t!cvf'):
+        message_content = message.content.split()
+        arg = message_content[1]
 
-            lst = arg.split("=")
-            for x in lst:
-                if "+" in x:
-                    y = lst[0]
-                    temp=lst[1].split("+")
-                    mx = temp[0]
-                    answer = temp[1]
-                elif "-" in x:
-                    y = lst[0]
-                    temp=lst[1].split("-")
-                    mx = temp[0]
-                    answer = temp[1]
-            if "+" in arg:
-                cvf_answer="Youre ansswer do be: {} - {} = {} :flushed:".format(mx, y, answer)
-            else:
-                cvf_answer="Youre ansssswer do be: {} + {} = {} :flushed:".format(mx, y, answer)
-            await message.channel.send(cvf_answer)
+        lst = arg.split("=")
+        for x in lst:
+            if "+" in x:
+                y = lst[0]
+                temp=lst[1].split("+")
+                mx = temp[0]
+                answer = temp[1]
+            elif "-" in x:
+                y = lst[0]
+                temp=lst[1].split("-")
+                mx = temp[0]
+                answer = temp[1]
+        if "+" in arg:
+            cvf_answer="Youre ansswer do be: {} - {} = {} :flushed:".format(mx, y, answer)
+        else:
+            cvf_answer="Youre ansssswer do be: {} + {} = {} :flushed:".format(mx, y, answer)
+        await message.channel.send(cvf_answer)
 
     @client.event
     async def on_ready():
