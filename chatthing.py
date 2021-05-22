8bd2aa4737aecd5bbfea9d6cf84286ba3237e15f
zTheroy <83382093+zTheroy@users.noreply.github.com>
2021-05-22 18:16:18 -0500

    fixed status 2: eletric bogaloo
    woa

--- chatthing.py
+++ chatthing.py
@@ -27,7 +27,7 @@ def main(argv):
 
     @client.event
     async def on_ready():
-        await client.change_presence(activity=discord.Activity(type=discord.Streaming(), name='eating baby powder | t!help for help'))
+        await client.change_presence(activity=discord.Streaming(name='eating baby powder | t!help for help', url='https://www.twitch.tv/ztheroy')
         
     @client.event
     async def on_message(message):
