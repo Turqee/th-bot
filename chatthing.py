e948feeec60b68765454f369e22c3f8f1441e527
zTheroy <83382093+zTheroy@users.noreply.github.com>
2021-07-05 18:58:36 -0500

    did status thing
    and added a new help command trigger (t!)

--- chatthing.py
+++ chatthing.py
@@ -23,7 +23,7 @@ def main(argv):
     @client.event
     async def on_ready():
       #  print('made it to client.event') (debug)
-        await client.change_presence(activity=discord.Streaming(name='balls | t!help', url='https://www.twitch.tv/ztheroy'))
+        await     await client.change_presence(activity=discord.Game('in a midwest tornado | t!help'))
         print("We have logged  in as {}".format(client))
     
     @client.event
@@ -132,7 +132,16 @@ def main(argv):
             github_title = discord.Embed(title="Code for Bot",  description="Steal the code for this bot! (Programmed in python 3+)", color=0xd9cf14)
             github_title.add_field(name="Github", value="https://github.com/zTheroy/theroy-discord-bot", inline=False)
             await message.channel.send(embed=github_title)
-        
+
+        if message.content.startswith('t!'):
+            info2 = discord.Embed(title="Commands list:",  description="All existing commands", color=0x28e038)
+            info2.add_field(name="t!inspiration", value="makes you go :O", inline=False)
+            info2.add_field(name="t!randommath", value="random math. adds numbers -45000-45000", inline=False)
+            info2.add_field(name="t!randomquotes", value="random quotes, made by yours truly", inline=False)
+            info2.add_field(name="t!cvf y=mx+b", value="replace y=mx+b with numbers (Don't make y or x a number, and don't add spaces)", inline=False)
+            info2.add_field(name="t!github", value="visit my github and steal this bots code (python 3+)", inline=False)
+            info2.add_field(name="t!ping", value="would send the time it takes to run the code but it doeSNT WORK AND ITS 2AM AND I HAVENT SLEPT GOD", inline=False)
+            await message.channel.send(embed=info)
 
 
     client.run(TOKEN)
