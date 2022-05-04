fb593dd701d2b14d589bfc94a47ca174dd27c78f
Theree <83382093+Therqy@users.noreply.github.com>
2022-05-03 20:16:10 -0500

    Large update (kind of)
    - Dark mode preference now works across all pages! (Light mode is a little buggy)
    - Made some text look pretty
    - Invite ThBot button is now a whole link and not just the text
    - Centered some images on the Dashboard page

--- website shit/javascript/main.js
+++ website shit/javascript/main.js
@@ -1,22 +1,31 @@
-// Dark mode function button
-function darkMode() {
- var element = document.body;
-   element.classList.toggle("dark-mode");
- }
-
- // Decides the theme based on the users system preference
-    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-    if (prefersDarkScheme.matches) {
-      document.body.classList.add("dark-mode");
-    }
-    else {
-      document.body.classList.remove("dark-mode");
-    }
-
     // Date feature
     n =  new Date();
     y = n.getFullYear();
     m = n.getMonth() + 1;
     d = n.getDate();
     document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+
+    const btn = document.querySelector(".btn-toggle");
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const currentTheme = localStorage.getItem("theme");
+    if (currentTheme == "dark") {
+      document.body.classList.toggle("dark-theme");
+    } else if (currentTheme == "light") {
+      document.body.classList.toggle("light-theme");
+    }
+
+    btn.addEventListener("click", function () {
+      if (prefersDarkScheme.matches) {
+        document.body.classList.toggle("light-theme");
+        var theme = document.body.classList.contains("light-theme")
+          ? "light"
+          : "dark";
+      } else {
+        document.body.classList.toggle("dark-theme");
+        var theme = document.body.classList.contains("dark-theme")
+          ? "dark"
+          : "light";
+      }
+      localStorage.setItem("theme", theme);
+    });
